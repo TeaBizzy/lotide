2dd30e43d93ccfd04bfd09f1737947905d08f040
Stefan <satalbot@protonmail.com>
2022-10-18 21:17:16 +0000

    Added assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(firstArray, secondArray) {
+  if(firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for(let i = 0; i < firstArray.length; i++) {
+    if(firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅Assertion Passed: [${arrayOne}] === [${arrayTwo}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`);
+  }
+};
+
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertArraysEqual([], ["1", "2", 3]) // => false
