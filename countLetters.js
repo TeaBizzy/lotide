fc19890dfabf817b4092181186dda041a3177cd5
Stefan <satalbot@protonmail.com>
2022-10-19 22:02:11 +0000

    Counts the number of times a character appears in a string, and returns the values as an object

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+
+  for (const char of str) {
+    if (results[char]) {
+      results[char] ++;
+    } else {
+      results[char] = 1;
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+const testString = "I'm learning so much at Lighthouse Labs";
+
+const result = countLetters(testString);
+
+assertEqual(result["l"], 1);
+assertEqual(result["L"], 2);
+assertEqual(result["'"], 1);
