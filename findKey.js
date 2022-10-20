7f5e8e859a98753b3a1f04d1ae422be50acc807a
Stefan <satalbot@protonmail.com>
2022-10-20 22:43:16 +0000

    Finds the first key in an object based on the call back function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey  = function(object, callBack) {
+  for (const key in object) {
+    if (callBack(object[key])) return key;
+  }
+};
+
+// TEST CODE
+const testObj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(testObj, key => key.stars === 2), "noma");
