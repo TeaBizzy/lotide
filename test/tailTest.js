51033c0fe23c5f8b48b19bc4f301130d37fd5e37
Stefan <satalbot@protonmail.com>
2022-10-24 18:33:46 +0000

    Changed tailTest.js to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,25 @@
-const assertEqual = require("../assertEqual");
+const { expect } = require("chai");
 const tail = require("../tail");
 
-// TEST CODE
-const words = ["Ballons", "Lighthouse", "Labs", "Foo", "Bar"];
-assertEqual(tail(words).length, words.length - 1);
-assertEqual(tail(words)[0], "Lighthouse");
+describe("#tail()", () => {
+  it("should return [\"Lighthouse\", \"Labs\", \"Foo\", \"Bar\"] for [\"Ballons\", \"Lighthouse\", \"Labs\", \"Foo\", \"Bar\"]", () => {
+    const input = ["Ballons", "Lighthouse", "Labs", "Foo", "Bar"];
+    const output = ["Lighthouse", "Labs", "Foo", "Bar"];
+
+    expect(tail(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [] for [\"Lighthouse\"]", () => {
+    const input = ["Lighthouse"];
+    const output = [];
+
+    expect(tail(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [] for []", () => {
+    const input = [];
+    const output = [];
+
+    expect(tail(input)).to.be.deep.equal(output);
+  });
+});
