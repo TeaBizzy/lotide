6f07953a1c98bbc5ddd4646052180d0eabd52e72
Stefan <satalbot@protonmail.com>
2022-10-24 23:09:41 +0000

    removed unnecessary test files. Added test for eqArrays.js

--- test/assertEqualTest.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = require("../assertEqual");
-
-assertEqual("Lighthouse Labs", "Bootcamp"); // false
-
-assertEqual(1, 1); // true
-
-assertEqual("My name is Stefan", "My name is Stefan"); // true
-
-assertEqual(20, 15); // false
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,15 +1,37 @@
-const assertEqual = require("../assertEqual");
+const { expect } = require("chai");
 const eqArrays = require("../eqArrays");
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+describe("#eqArrays()", () => {
+  it("should return true for [1, 2, 3], [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    
+    expect(eqArrays(input, input)).to.be.equal(true);
+  });
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+  it("should return false for [\"1\", \"2\"], [1, 2]", () => {
+    const input1 = ["1", "2"];
+    const input2 = [1, 2];
+    
+    expect(eqArrays(input1, input2)).to.be.equal(false);
+  });
 
-// Nested Arrays
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+  it("should return false for [true, true, true], [true, true]", () => {
+    const input1 = [true, true, true];
+    const input2 = [true, true];
+    
+    expect(eqArrays(input1, input2)).to.be.equal(false);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+  it("should return true for [[2, 3], 1], [[2, 3], 1]]", () => {
+    const input = [[2, 3], 1];
+    
+    expect(eqArrays(input, input)).to.be.equal(true);
+  });
+
+  it("should return false for [[1]], [1]", () => {
+    const input1 = [[1]];
+    const input2 = [1];
+    
+    expect(eqArrays(input1, input2)).to.be.equal(false);
+  });
+});
