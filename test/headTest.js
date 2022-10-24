69ca347feaa4561f667ec8e17018d40a2facd711
Stefan <satalbot@protonmail.com>
2022-10-24 18:10:40 +0000

    Modified headTest.js to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,40 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
+const { expect } = require("chai");
 const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 6);
-assertEqual(head([]), undefined);
+describe("#head()", () => {
+  it("should return 1 for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const output = 1;
+
+    expect(head(input)).to.be.equal(output);
+  });
+
+  it("should return 5 for [5]", () => {
+    const input = [5];
+    const output = 5;
+
+    expect(head(input)).to.be.equal(output);
+  });
+
+  it("should return 5 for [5, 6, 7]", () => {
+    const input = [5, 6, 7];
+    const output = 5;
+
+    expect(head(input)).to.be.equal(output);
+  });
+
+  it("should return \"Hello\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const output = "Hello";
+
+    expect(head(input)).to.be.equal(output);
+  });
+
+  it("should return undefined for []", () => {
+    const input = [];
+    const output = undefined;
+
+    expect(head(input)).to.be.equal(output);
+  });
+});
