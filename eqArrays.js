c05574a8e09a5686acfa1bd8f487f3c7aa2ac827
Stefan <satalbot@protonmail.com>
2022-10-24 02:12:44 +0000

    Added support for comparing nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,12 @@
 // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log("✅✅✅Assertion Passed: ", arrayOne, " === ", arrayTwo); // Do not use template literals, they cause arrays to display improperly.
@@ -13,11 +21,17 @@ const eqArrays = function(firstArray, secondArray) {
   }
 
   for (let i = 0; i < firstArray.length; i++) {
+    if (Array.isArray(firstArray[i])) {
+      if (eqArrays(firstArray[i], secondArray[i]) === false) {
+        return false;
+      } else {
+        continue;
+      }
+    }
     if (firstArray[i] !== secondArray[i]) {
       return false;
     }
   }
-
   return true;
 };
 
@@ -27,3 +41,9 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// Nested Arrays
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
