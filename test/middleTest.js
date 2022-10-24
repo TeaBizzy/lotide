40619c5e4788e5e6590bef5ba282ba3b32498850
Stefan <satalbot@protonmail.com>
2022-10-24 18:22:51 +0000

    Modify test to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,40 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const { expect } = require("chai");
 const middle = require("../middle");
 
 // TEST CODE
-const testOddArray = [1, 2, 3];
-const testEvenArray = [true, false, false, true];
-const testEvenArrayTwo = [true, false, false, true, false, true, true, true];
-const testSmallArray = ["A", "B"];
+describe("#middle()", () => {
+  it("should return [1] for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const output = [1];
 
-assertArraysEqual(middle(testOddArray), [1]); // true
-assertArraysEqual(middle(testEvenArray), [1, 2]); // true
-assertArraysEqual(middle(testEvenArrayTwo), [3, 4]); // true
-assertArraysEqual(middle(testSmallArray), []); // true
+    expect(middle(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [1, 2] for [true, false, false, true]", () => {
+    const input = [true, false, false, true];
+    const output = [1, 2];
+
+    expect(middle(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [0] for [3, 4]", () => {
+    const input = [3, 4];
+    const output = [];
+
+    expect(middle(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [] for [1]", () => {
+    const input = [1];
+    const output = [];
+
+    expect(middle(input)).to.be.deep.equal(output);
+  });
+
+  it("should return [] for []", () => {
+    const input = [];
+    const output = [];
+
+    expect(middle(input)).to.be.deep.equal(output);
+  });
+});
